27a1210d7dd4e5085984676769192a9d7328a41c
gael rubin <gaelrubin@MacBook-Air-de-gael.local>
2022-11-14 15:33:47 +0100

    probleme construct copie

--- main.cpp
+++ main.cpp
@@ -297,81 +297,91 @@ int main()
 
 
     std::cout << "...................MAP......................." << std::endl << std::endl;
-
-
-    std::cout << "..........INSERT AND BEGIN AND END..........." << std::endl << std::endl;
+    std::cout << "...............CONSTRUCTOR..................." << std::endl << std::endl; 
     std::map <std::string, int> std_map1;
     ft::map <std::string, int> ft_map1;
-    std::map <std::string, int> std_map2(std_map1);
-    ft::map <std::string, int> ft_map2(ft_map1);
     std_map1["cat"] = 1;
     std_map1["chien"] = 2;
-    std_map1.insert(std::make_pair("chat", 3));
     ft_map1["cat"] = 1;
     ft_map1["chien"] = 2;
-    ft_map1.insert(ft::make_pair("chat", 3));
+    std::map <std::string, int> std_map2(std_map1.begin(), std_map1.end());
+    ft::map <std::string, int> ft_map2(ft_map1.begin(), ft_map1.end());
+    std::map <std::string, int> std_map3(std_map2);
+    ft::map <std::string, int> ft_map3(ft_map2);
     std::map<std::string, int>::iterator it;
-    for(it = std_map1.begin(); it != std_map1.end(); it++)
+    for(it = std_map3.begin(); it != std_map3.end(); it++)
         std::cout << it->first << " => " << it->second << std::endl;
     ft::map<std::string, int>::iterator it2;
-    for(it2 = ft_map1.begin(); it2 != ft_map1.end(); it2++)
+    for(it2 = ft_map3.begin(); it2 != ft_map3.end(); it2++)
         std::cout << it2->first << " => " << it2->second << std::endl;
-/*
     std::cout << std::endl;
+    std::cout << "..........INSERT AND BEGIN AND END..........." << std::endl << std::endl;
+
+    std_map3.insert(std::make_pair("chat", 3));
+    ft_map3.insert(ft::make_pair("chat", 3));
+    for(it = std_map3.begin(); it != std_map3.end(); it++)
+        std::cout << it->first << " => " << it->second << std::endl;
+    for(it2 = ft_map3.begin(); it2 != ft_map3.end(); it2++)
+        std::cout << it2->first << " => " << it2->second << std::endl;
+    std::cout << std::endl;
+
     std::cout << "..............SIZE AND MAXSIZE.............." << std::endl << std::endl;    
-    std::cout << "std_map1 = " << std_map1.size() << std::endl;
-    std::cout << "ft_map2 = " << ft_map1.size() << std::endl;
+    std::cout << "std_map1 = " << std_map3.size() << std::endl;
+    std::cout << "ft_map2 = " << ft_map3.size() << std::endl;
     std::cout << std::endl;
-    std::cout << "std_map1 = " << std_map1.max_size() << std::endl;
-    std::cout << "ft_map1 = " << ft_map1.max_size() << std::endl;
-    std::cout << std::endl;    
+    std::cout << "std_map1 = " << std_map3.max_size() << std::endl;
+    std::cout << "ft_map1 = " << ft_map3.max_size() << std::endl;
+    std::cout << std::endl;
 
     std::cout << "....................SWAP...................." << std::endl << std::endl;
-    std::map <std::string, int> std_map2;
-    ft::map <std::string, int> ft_map2;
-    std_map2["dog"] = 1;
-    std_map2["oiseau"] = 2;
-    std_map2.insert(std::make_pair("poulet", 3));
-    ft_map2["dog"] = 1;
-    ft_map2["oiseau"] = 2;
-    ft_map2.insert(ft::make_pair("poulet", 3));
-    for(it = std_map1.begin(); it != std_map1.end(); it++)
-        std::cout << "std_map1 = " << it->first << " => " << it->second << std::endl;
-    std_map1.swap(std_map2);
-    for(it = std_map1.begin(); it != std_map1.end(); it++)
-        std::cout << "std_map1 = " << it->first << " => " << it->second << std::endl;
+    std::map <std::string, int> std_map4;
+    ft::map <std::string, int> ft_map4;
+    std_map4["dog"] = 11;
+    std_map4["oiseau"] = 22;
+    ft_map4["dog"] = 11;
+    ft_map4["oiseau"] = 22;
+    for(it = std_map3.begin(); it != std_map3.end(); it++)
+        std::cout << "std_map3 avant swap = " << it->first << " => " << it->second << std::endl;
+    std_map3.swap(std_map4);
+    for(it = std_map3.begin(); it != std_map3.end(); it++)
+        std::cout << "std_map3 après swap = " << it->first << " => " << it->second << std::endl;
     std::cout << std::endl;
-    for(it2 = ft_map1.begin(); it2 != ft_map1.end(); it2++)
-        std::cout << "ft_map1 = " << it2->first << " => " << it2->second << std::endl;
-    ft_map1.swap(ft_map2);
-    for(it2 = ft_map1.begin(); it2 != ft_map1.end(); it2++)
-        std::cout << "ft_map1 = " << it2->first << " => " << it2->second << std::endl;
+    for(it2 = ft_map3.begin(); it2 != ft_map3.end(); it2++)
+        std::cout << "ft_map3 avant swap = " << it2->first << " => " << it2->second << std::endl;
+    ft_map3.swap(ft_map4);
+    for(it2 = ft_map3.begin(); it2 != ft_map3.end(); it2++)
+        std::cout << "ft_map3 après swap = " << it2->first << " => " << it2->second << std::endl;
     std::cout << std::endl;
-
+/*
     std::cout << "..................ERASE....................." << std::endl << std::endl;
-    std_map1.erase(std_map1.begin());
-    for(it = std_map1.begin(); it != std_map1.end(); it++)
-        std::cout << "std_map1 = " << it->first << " => " << it->second << std::endl;
-    ft_map1.erase(ft_map1.begin());
-    for(it2 = ft_map1.begin(); it2 != ft_map1.end(); it2++)
-        std::cout << "ft_map1 = " << it2->first << " => " << it2->second << std::endl;
+    std_map3.erase(std_map3.begin());
+    for(it = std_map3.begin(); it != std_map3.end(); it++)
+        std::cout << "std_map3 = " << it->first << " => " << it->second << std::endl;
+    ft_map3.erase(ft_map3.begin());
+    for(it2 = ft_map3.begin(); it2 != ft_map3.end(); it2++)
+        std::cout << "ft_map3 = " << it2->first << " => " << it2->second << std::endl;
     std::cout << std::endl;
-    std_map1.erase("poulet");
-    for(it = std_map1.begin(); it != std_map1.end(); it++)
-        std::cout << "std_map1 = " << it->first << " => " << it->second << std::endl;
-    ft_map1.erase("poulet");
-    for(it2 = ft_map1.begin(); it2 != ft_map1.end(); it2++)
-        std::cout << "ft_map1 = " << it2->first << " => " << it2->second << std::endl;
-    std_map1.erase(std_map1.begin(), std_map1.end());
-    ft_map1.erase(ft_map1.begin(), ft_map1.end());
+    std_map3.erase("oiseau");
+    for(it = std_map3.begin(); it != std_map3.end(); it++)
+        std::cout << "std_map3 = " << it->first << " => " << it->second << std::endl;
+    ft_map3.erase("oiseau");
+    for(it2 = ft_map3.begin(); it2 != ft_map3.end(); it2++)
+        std::cout << "ft_map3 = " << it2->first << " => " << it2->second << std::endl;
+    std_map4.erase(std_map4.begin(), std_map4.end());
+    for(it = std_map4.begin(); it != std_map4.end(); it++)
+        std::cout << "std_map4 = " << it->first << " => " << it->second << std::endl;
+    ft_map4.erase(ft_map4.begin(), ft_map4.end());
+    for(it2 = ft_map4.begin(); it2 != ft_map4.end(); it2++)
+        std::cout << "ft_map4 = " << it2->first << " => " << it2->second << std::endl;
     std::cout << std::endl; 
-    std::cout << "std_map1 = " << std_map1.empty() << std::endl;
-    std::cout << "ft_map1 = " << ft_map1.empty() << std::endl;
+    std::cout << "std_map3 = " << std_map1.empty() << std::endl;
+    std::cout << "ft_map3 = " << ft_map1.empty() << std::endl;
+    std::cout << "std_map4 = " << std_map1.empty() << std::endl;
+    std::cout << "ft_map4 = " << ft_map1.empty() << std::endl;
     std::cout << std::endl;        
-
 */
 
-/*
+
     std::cout << "..................STACK....................." << std::endl << std::endl;
 
     std::cout << "..................EMPTY....................." << std::endl << std::endl;
@@ -406,6 +416,6 @@ int main()
     std::cout << "...................SIZE....................." << std::endl << std::endl;
     std::cout << "std_stack = " << std_stack.size() << std::endl;
     std::cout << "ft_stack = " << ft_stack.size() << std::endl;
-*/
+
     return (0);
 }
